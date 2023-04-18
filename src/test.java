8be4a3e496ebf6c70659903f65e69e7d3c855fa7
Merey <misenbai@gmail.com>
2023-04-18 17:03:17 +0600

    8th

---
+++ src/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+        MyList<Integer> myList = new MyArrayList();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        System.out.println("Elements in the list after adding 3 elements: " + myList.size());
+        System.out.println("Element at index 1: " + myList.get(1));
+    }
+}
