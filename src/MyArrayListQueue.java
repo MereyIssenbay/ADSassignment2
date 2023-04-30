948f855c4cdab296d6a34aff07e200c25ee26e82
Merey <misenbai@gmail.com>
2023-04-30 13:13:47 +0600

    assignment3

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,4 +1,8 @@
 import java.util.NoSuchElementException;
-public class MyArrayListQueue {
+public class MyArrayListQueue<E> {
     private MyArrayList<E> array;
+    public MyArrayListQueue() {
+        array = new MyArrayList<E>();
+    }
+
 }
